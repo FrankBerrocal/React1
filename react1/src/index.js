44ff882e5035b7b1cd4dbb9e2f40e943870ec6c8
FrankBerrocal <f.berrocalazofeif887@mybvc.ca>
2023-10-16 16:21:37 -0700

    Reviewing JS

--- react1/src/index.js
+++ react1/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+var =  dont use it.
+let =block only, dunamic value.
+const = block only, static value.
+function sayHello(){
+    for (var i =0; i<5; i++){
+        console.log(i);
+    }
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+sayHello();*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const person = {
+    name: "Frank",
+    walk() {},
+    talk(){}
+}
+
+person.talk();
+person['name'] = 'John';
+
+//use the code dynamically
+
+const username = 'name';
+person[username] = 'Frankie';
+
+console.log(person.name);
